5c92145cd163b0b394b4043f23770eac1c40fe9c
arpit-agr <arpit.agrawal2502@gmail.com>
2026-07-08 17:45:12 +0530

    Update socials.js

--- src/_data/socials.js
+++ src/_data/socials.js
@@ -1,17 +1,27 @@
 export default [
 	{
-		name: "GitHub",
-		url: "https://github.com/arpit-agr",
-		icon: "github",
-	},
-	{
-		name: "CodePen",
-		url: "https://codepen.io/arpit-codes",
-		icon: "codepen",
-	},
-	{
 		name: "Bluesky",
 		url: "https://bsky.app/profile/arpit.codes",
 		icon: "bluesky",
 	},
+	{
+		name: "Mastodon",
+		url: "https://indieweb.social/@arpit",
+		icon: "mastodon",
+	},
+	{
+		name: "LinkedIn",
+		url: "https://www.linkedin.com/in/arpit-codes/",
+		icon: "linkedin",
+	},
+	// {
+	// 	name: "GitHub",
+	// 	url: "https://github.com/arpit-agr",
+	// 	icon: "github",
+	// },
+	// {
+	// 	name: "CodePen",
+	// 	url: "https://codepen.io/arpit-codes",
+	// 	icon: "codepen",
+	// },
 ];
